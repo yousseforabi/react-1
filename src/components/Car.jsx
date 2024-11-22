9eadeaa8ba44bdaf3a6d08bbb465cfa8e7164b52
yousseforabi <122828573+yousseforabi@users.noreply.github.com>
2024-11-22 23:30:00 +0100

    Adding CSS

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -1,32 +1,41 @@
+
 import mercedesCar from '../assets/images/mercedes.jpeg';
 import ferrariCar from '../assets/images/ferrari.jpeg'; 
 import hondaCar from '../assets/images/honda.jpeg';
 import jeepCar from '../assets/images/jeep.jpeg';
 import fordCar from '../assets/images/ford.jpeg';
+import "../components/Car.css";
 
 function Car() {
     return (
-        <>
-        <img src={mercedesCar} alt="mercedes" />
-        <h1>Hello, Mercedes!</h1>
-        <p>price is 2000 SEK</p>
-        <img src={ferrariCar} alt="ferarri"/>
-        <h1>Hello, Ferrari!</h1>
-        <p>price is 35000 SEK</p>
-        <img src={fordCar} alt="ford"/>
-        <h1>Hello, Ford!</h1>
-        <p>price is 15000 SEK</p>
-        <img src={jeepCar} alt="jeep"/>
-        <h1>Hello, Jeep!</h1>
-        <p>price is 12000 SEK</p>
-        <img src={hondaCar} alt="bmw"/>
-        <h1>Hello, Honda!</h1>
-        <p>price is 18000 SEK</p>
-
-        </>
-        
-      )
-
+        <div className="car-showcase">
+            <div className="car-item">
+                <img src={mercedesCar} alt="Mercedes" />
+                <h1 className="car-model">Hello, Mercedes!</h1>
+                <p>Price: 2000 SEK</p>
+            </div>
+            <div className="car-item">
+                <img src={ferrariCar} alt="Ferrari"/>
+                <h1 className="car-model">Hello, Ferrari!</h1>
+                <p>Price: 35000 SEK</p>
+            </div>
+            <div className="car-item">
+                <img src={fordCar} alt="Ford"/>
+                <h1 className="car-model">Hello, Ford!</h1>
+                <p>Price: 15000 SEK</p>
+            </div>
+            <div className="car-item">
+                <img src={jeepCar} alt="Jeep"/>
+                <h1 className="car-model">Hello, Jeep!</h1>
+                <p>Price: 12000 SEK</p>
+            </div>
+            <div className="car-item">
+                <img src={hondaCar} alt="Honda"/>
+                <h1 className="car-model">Hello, Honda!</h1>
+                <p>Price: 18000 SEK</p>
+            </div>
+        </div>
+    );
 }
 
 export default Car;
