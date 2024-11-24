ac904f6132c67ccb3752d325ba09bd7ba714d6ca
yousseforabi <122828573+yousseforabi@users.noreply.github.com>
2024-11-24 22:28:25 +0100

    adding props

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -6,33 +6,33 @@ import jeepCar from '../assets/images/jeep.jpeg';
 import fordCar from '../assets/images/ford.jpeg';
 import "../components/Car.css";
 
-function Car() {
+function Car(props) {
     return (
         <div className="car-showcase">
             <div className="car-item">
                 <img src={mercedesCar} alt="Mercedes" />
-                <h1 className="car-model">Hello, Mercedes!</h1>
-                <p>Price: 2000 SEK</p>
+                <h1 className="car-model">{props.model}</h1>
+                <p>Price: {props.price}SEK</p>
             </div>
             <div className="car-item">
                 <img src={ferrariCar} alt="Ferrari"/>
-                <h1 className="car-model">Hello, Ferrari!</h1>
-                <p>Price: 35000 SEK</p>
+                <h1 className="car-model">{props.model}</h1>
+                <p>Price: {props.price} SEK</p>
             </div>
             <div className="car-item">
                 <img src={fordCar} alt="Ford"/>
-                <h1 className="car-model">Hello, Ford!</h1>
-                <p>Price: 15000 SEK</p>
+                <h1 className="car-model">{props.model}</h1>
+                <p>Price: {props.price} SEK</p>
             </div>
             <div className="car-item">
                 <img src={jeepCar} alt="Jeep"/>
-                <h1 className="car-model">Hello, Jeep!</h1>
-                <p>Price: 12000 SEK</p>
+                <h1 className="car-model">{props.model}</h1>
+                <p>Price: {props.price} SEK</p>
             </div>
             <div className="car-item">
                 <img src={hondaCar} alt="Honda"/>
-                <h1 className="car-model">Hello, Honda!</h1>
-                <p>Price: 18000 SEK</p>
+                <h1 className="car-model">{props.model}</h1>
+                <p>Price: {props.price} SEK</p>
             </div>
         </div>
     );
