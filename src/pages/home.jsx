e0f1366f3a9ce875bf55c06af21b7616f8e44dd1
yousseforabi <122828573+yousseforabi@users.noreply.github.com>
2024-11-26 22:44:43 +0100

    adding set price and car name

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,24 +1,25 @@
 import Car from '../components/Car.jsx';
 import { useState } from 'react';
 function home(options) {
+    const [carName,setName] = useState("FORD");
+    const [carPrice, setPrice] = useState(0);
+    const updateName = () =>{
+     setName("FORD");
+ 
+    }
+    const updatePrice = () =>{
+     setPrice(carPrice +1);
+    }
 
-    return <>
+    return (<>
     <Car model1="Mercedes" price1="2300" 
     model2="FERARRI" price2="2300"
      model3="HINDA" price3="2300" 
      model4="FORD" price4="2300"
      model5="KIA" price5="2300"/>
-   </>
-   const [carName,setName] = useState("FORD");
-   const [carPrice, setPrice] = useState(0);
-   const updateName = () =>{
-    setName("FORD");
+   
+  
 
-   }
-   const updatePrice = () =>{
-    setPrice(carPrice+1);
-   }
-   return (
     <div>
         <button className='home-btn' onClick={updateName}></button>
       <h2>CAR NAME</h2>
@@ -26,6 +27,7 @@ function home(options) {
         <h2>CAR PRICE</h2>
       </button>
     </div>
+    </>
    )
 }
 export default home;
